048a7842b9f98e7a9522b893e2f872888bdbd72b
fei li <46519353+LfLab-AI@users.noreply.github.com>
2025-08-04 16:55:12 +0800

    Update long_testprofile.sh

--- long_testprofile.sh
+++ long_testprofile.sh
@@ -1,6 +1,6 @@
 gpuid=0
 group_size=32
-model='/home/dell/lf/lf/model/meta-llama/Llama-2-7b-hf' ##'/home/dell/lf/lf/model/Mistral-7B-Instruct-v0.3' ##'/home/dell/lf/lf/model/meta-llama/Llama-2-7b-hf' ##'/home/dell/lf/lf/model/Mistral-7B-Instruct-v0.3' ##'/home/dell/lf/lf/model/Meta-Llama-3-8B'
+model='/path/your_model'
 
 CUDA_VISIBLE_DEVICES=$gpuid python pred_long_benchprofile.py  --model_name_or_path $model \
     --cache_dir ./cached_models \
